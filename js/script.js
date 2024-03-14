003077f2e32c3fa8f4c84406fbe92d836371d922
Hektor Bashota <120386001+itshektor@users.noreply.github.com>
2024-03-14 23:01:53 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,245 @@
+
+const typed = new Typed('.multiple',{
+
+    strings: ['Programmer', 'Frontend Developer', 'Student'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+/*
+let firstname;
+firstname = 'Hektor';
+
+let lastname = 'Bashota';
+var mosha = 16;
+
+console.log(firstname);
+document.write(lastname);
+alert(mosha);
+
+let a = 30; 
+if(a > 40){
+    document.write("Vlera e a eshte me e madhe se 40;");
+} else if(a<40){
+    document.write("Vlera e a eshte me e vogel se 40;");
+}else{
+    document.write("Vlera e a eshte e barabarte me 40");
+}
+
+let age = 18;
+
+if(age <= 13){
+    console.log('Ti je nje femije');
+}else if(age<=18){
+    console.log('Ti je nje adoleshent');
+}else if(age<=65){
+    console.log('Ti je nje i rritur');
+}else if(age>65 && age<110){
+    console.log('Ti je nje i moshuar');
+}else{
+    console.log("Invalid number");
+}
+
+*/
+//detyra 1
+/*
+let numri1 = 7;
+let numri2 = 5;
+
+if(numri1>numri2){
+    alert("Numri i pare eshte me i madh");
+}else if(numri2>numri1){
+    alert("Numri i dyte eshte me i madhe");
+}else if(numri1===numri2){
+    alert("Numrat jane te barabarte");
+}else{
+    alert("invalid input");
+}
+
+
+let nota;
+const piket = prompt("Shkruaj piket");
+if(piket<0){
+    alert("something is wrong")
+}else if(piket <= 50){
+    nota = 1;
+    alert(nota);
+}else if(piket <= 60){
+    nota = 2;
+    alert(nota);
+}else if(piket <= 70){
+    nota = 3;
+    alert(nota);
+}else if(piket<=85){
+    nota = 4;
+    alert(nota);
+}else if(piket<=100){
+    nota = 5;
+    alert(nota);
+}else if(piket>=101){
+    alert("something is wrong");
+}
+
+let fruit = 'apple';
+switch(fruit){
+    case 'banana':
+        console.log('yellow fruit');
+        break;
+    case 'apple':
+        console.log('round fruit');
+        break;
+    case 'orange':
+        console.log('citrus fruit');
+        break;
+    default:
+        console.log('fruit not found');
+}
+
+const day = prompt("Jepni numrin e dites se javes: ");
+
+switch(day){
+    case '1':
+        alert('Monday');
+        break;
+    case '2':
+        alert('Tuesday');
+        break;
+    case '3':
+        alert('Wednesday');
+        break;
+    case '4':
+        alert('Thursday');
+        break;
+    case '5':
+        alert('Friday');
+        break;
+    case '6':
+        alert('Saturday');
+        break;
+    case '7':
+        alert('Sunday');
+        break;
+    default:
+        alert('invalid input');
+        break;
+}
+/*
+const ngjyra = prompt("Jep ngjyren: ");
+switch(ngjyra){
+    case 'red':
+    case 'blue':
+        alert("Ngjyre e bukur");
+        break;
+    case 'yellow':
+    case 'green':
+        alert("Ngjyre interesant");
+        break;
+    case 'black':
+    case 'white':
+        alert("Ngjyre e thjeshte");
+        break;
+    default:
+        alert('Invalid input of color');
+        break;
+}*/
+/*
+const ngjyra = prompt("Jep nje ngjyre: ");
+if(ngjyra === 'red' || ngjyra === 'blue' ){
+    alert('Ngjyre e preferuar');
+}else if(ngjyra === 'black' || ngjyra === 'white'){
+    alert('Ngjyre e thjeshte');
+}else if(ngjyra === 'yellow' || ngjyra === 'green'){
+    alert('Ngjyre interesant')
+}
+
+const kafe = prompt('What type of coffee do you want to drink: ');
+switch(kafe){
+    case 'espresso':
+        alert('Ingredients: Just Coffee');
+        break;
+    case 'makiato':
+        alert('Ingredients: Coffee + Milk');
+        break;
+    case 'amerikane':
+        alert('Ingredients: Coffee + Water');
+        break;
+    case 'latte':
+        alert("We don't have latte");
+        break;
+    case 'cappucino':
+        alert("We don't have cappucino");
+        break;
+    default:
+        alert('invalid input');
+        break;
+}
+*/
+/*
+const grade = prompt('Whats ur grade: ');
+switch(grade){
+    case 'A':
+        alert('Excellent!');
+        break;
+    case 'B':
+        alert('Good');
+        break;
+    case 'C':
+        alert('Average');
+        break;
+    case 'D':
+        alert('Poor');
+        break;
+    case 'F':
+        alert('Fail');
+        break;
+    default:
+        alert('invalid');
+        break;
+}
+
+if(grade === 'A'){
+    alert('Excellent!');
+}else if(grade === 'B'){
+    alert('Good');
+}else if(grade === 'C'){
+    alert('Average');
+}else if(grade === 'D'){
+    alert('Poor');
+}else if(grade === 'F'){
+    alert('Fail');
+}else(
+    alert('invalid')
+)*/
+/*
+for(let i = 1; i<=10; i++){
+    console.log('Hello World!')
+}
+for(var i = 10; i<=1; i--){
+    console.log(i);
+}
+
+for(let i=0; i<= 10; i++){
+    if( i % 2 !==0){
+    console.log("hello",i);
+    }
+}
+*/
+/*
+let sum;
+let num = 3;
+for(let i=1; i<=10; i++ ){
+
+    sum = i*num;
+    console.log(sum);
+
+} */
+/*
+for(let i = 5; i >=1; i++){
+    let row = '';
+    for(let j = 1; j <=i; j--){
+        row += "* "
+    }
+    console.log(row)
+}
+*/
